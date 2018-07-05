a12bf7cb3034ccf8cb3ae79e5d269ca0fb112526
Eoghan Gallagher <eoghan.gallagher@gmail.com>
2018-07-05 17:01:44 +0100

    Added Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour 
+{
+	[SerializeField] private TextMeshProUGUI timerText;
+	[SerializeField] private bool isTimerRunning = false;
+	[SerializeField] private float startTime;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float t = Time.time - startTime;
+
+		string minutes = ( (int) t / 60 ).ToString(  ) ;
+		string seconds = ( t % 60 ).ToString( "F2" );
+
+		timerText.text = minutes + ":" + seconds;
+	}
+
+	public void StartTimer()
+	{
+		isTimerRunning = true;
+	}
+
+	public void StopTimer()
+	{
+		isTimerRunning  = false;
+	}
+}
