f085af281adff86dfb2045356b2b26a6c2ee732e
Eoghan <eoghan.gallagher@gmail.com>
2018-07-09 17:41:49 +0100

    Added Basic Session Manager for capturing user data

--- Assets/Scripts/BeginButton.cs
+++ Assets/Scripts/BeginButton.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SessionManager;
+using GameSessions;
 using Newtonsoft.Json;
 
 public class BeginButton : MonoBehaviour 
@@ -20,13 +20,22 @@ public class BeginButton : MonoBehaviour
 			t.TransitionNo = i;
 			t.TransistionName = "tr " + i;
 
+			for( int y = 0; y < 3; y++ )
+			{
+				Error e = new Error();
+				e.ErrorType = "bad!!";
+
+				t.transitionErrors.Add( e );
+			}
+
 			sess.transistions.Add( t );
 		}
-
 
 		for( int i = 0; i < 10; i++ )
 		{
 			Debug.Log( sess.transistions[i].TransistionName );
+
+			
 		}
 
 		var jsonString = JsonConvert.SerializeObject( sess );
--- Assets/Scripts/Session Manager/Session.cs
+++ Assets/Scripts/Session Manager/Session.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-namespace SessionManager
+namespace GameSessions
 {
 
 	[System.Serializable]
 	public class Session  
 	{
 
-		public string SessionName { get; set; }
-		public  List<Transition> transistions = new List<Transition>();
+
+		public string SessionName { get; set; } // Do i need this ?
+		public int SessionNumber { get; set; } //unique number
+		public string PlayerID { get; set; } //Which player?
+		public string GameID { get; set; } //which game ?
+		public string DeviceType { get; set; } //Device upon which game is running
+		public float SessionDuration { get; set; } //Duration of session
+		public string TimeStamp { get; set; } //Replace string with date time.
+		public bool SessionCompleted { get; set; } //Did the player complete the session
+
+		public  List<Transition> transistions = new List<Transition>(); //Number of transitions that occured during a session
 	}
 
 	public class Transition
@@ -19,8 +28,18 @@ namespace SessionManager
 		public int TransitionNo { get; set; }
 		public string TransistionName { get; set; }
 
-		public string srcButton { get; set; }
-		public string destButton{  get; set; }
+		public float TransitionTime{ get; set; }
+
+		public string SourceButton { get; set; }
+		public string DestinationButton{  get; set; }
+
+		public List <Error> transitionErrors = new List<Error>(); 
+	}
+
+
+	public class Error
+	{
+		public string ErrorType;
 	}
 
 }
