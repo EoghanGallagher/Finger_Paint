3c3cf3c1528cccf47767d52dcf13794cad698a36
Eoghan <eoghan.gallagher@gmail.com>
2018-07-09 17:00:01 +0100

    Added JsonNet for unity

---
+++ Assets/Scripts/BeginButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SessionManager;
+using Newtonsoft.Json;
+
+public class BeginButton : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		Session sess = new Session();
+
+		sess.SessionName = "First Session";
+
+		for( int i = 0; i < 10; i++ )
+		{
+			Transition t = new Transition();
+			t.TransitionNo = i;
+			t.TransistionName = "tr " + i;
+
+			sess.transistions.Add( t );
+		}
+
+
+		for( int i = 0; i < 10; i++ )
+		{
+			Debug.Log( sess.transistions[i].TransistionName );
+		}
+
+		var jsonString = JsonConvert.SerializeObject( sess );
+
+		Debug.Log( jsonString );
+
+
+		
+	}
+	
+	
+}
---
+++ Assets/Scripts/Session Manager/Session.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SessionManager
+{
+
+	[System.Serializable]
+	public class Session  
+	{
+
+		public string SessionName { get; set; }
+		public  List<Transition> transistions = new List<Transition>();
+	}
+
+	public class Transition
+	{
+		public int TransitionNo { get; set; }
+		public string TransistionName { get; set; }
+
+		public string srcButton { get; set; }
+		public string destButton{  get; set; }
+	}
+
+}
