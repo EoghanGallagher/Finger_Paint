ad9b91740bbec131a0cac97fb54d1142f9d52978
Eoghan <eoghan.gallagher@gmail.com>
2018-07-27 15:45:35 +0100

    Refactor Friday !

--- Assets/Scripts/LevelHandler.cs
+++ Assets/Scripts/LevelHandler.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+//Resonsible for managing the trail maker tests 
+//As of this build there are 4 test or levels
+//A level is a prefab that contains the assets and layout for a Trailmaker test
 public class LevelHandler : MonoBehaviour 
 {
-	[SerializeField] private  GameObject[] levelList;
+	[SerializeField] private  GameObject[] levelList; //List of levels
 	[SerializeField] int currentLevel = 0;
 
 
@@ -13,35 +17,42 @@ public class LevelHandler : MonoBehaviour
 	{
 		//PlayerPrefs.DeleteAll();
 	
+		//When the level is loaded
+		//Check what value the current level has 0 - 3 
 		if( PlayerPrefs.HasKey( "CurrentLevel" ) )
 		{
 			currentLevel = PlayerPrefs.GetInt( "CurrentLevel" );
 			Debug.Log( currentLevel );
 		}
 
+		//Activate the current level
 		SetActiveLevel();
 	}
 
 
+	//Load the scene level
 	public void LoadLevel()
 	{
-		SceneManager.LoadSceneAsync( "Trail_Making_Test_Part_A" );
+		SceneManager.LoadSceneAsync( "Star_Racer_Main" );
 	}
 
+
+	//Deactivate all levels
+	//Activate the current level
 	private void SetActiveLevel()
 	{
+		//Disable all level prefabs contained in the list
 		foreach( GameObject level in levelList )
 		{
 			level.SetActive( false );
 		}
-
+		//Enable the currently selected level
 		levelList[ currentLevel ].SetActive( true );
 	}
 
-
+	//Called by the next level buttons On click event 
 	public void SetNextLevel(  )
 	{
-		
 		if( currentLevel < levelList.Length -1  )
 			currentLevel ++;
 		else
