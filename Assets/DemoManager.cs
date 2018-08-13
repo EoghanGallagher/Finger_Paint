9a933c6e5da52ea979fb35b1d0a27dc6855f145a
Eoghan <eoghan.gallagher@gmail.com>
2018-08-13 17:39:42 +0100

    Added fade out to Demo Scene

--- Assets/DemoManager.cs
+++ Assets/DemoManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DemoManager : MonoBehaviour 
 {
@@ -11,14 +12,24 @@ public class DemoManager : MonoBehaviour
 
 	[SerializeField] private GameObject demoPointer;
 
+	[SerializeField] private  Image fader;
+
+
+
 	private TeleType teleType;
 	private StarScaler starScaler; 
+
+	private Color tmpAlpha; 
+
+	
 
 	// Use this for initialization
 	void Start () 
 	{
 		teleType = demoText.GetComponent<TeleType>();
 		starScaler = stars.GetComponent<StarScaler>();
+		
+		
 		
 		if( !demoText )
 		{
@@ -32,7 +43,7 @@ public class DemoManager : MonoBehaviour
 	
 	private IEnumerator DemoSequence()
 	{
-	
+		float tmp = 0;
 		yield return StartCoroutine( teleType.RevealText() ); //Display Demo Text
 
 		yield return new WaitForSeconds( 0.5f );
@@ -47,7 +58,21 @@ public class DemoManager : MonoBehaviour
 
 		//Return to Intro screen
 		yield return new WaitForSeconds( 15.0f );
+
+		
+		while( fader.color.a < 1 )
+		{
+			
+			yield return new WaitForSeconds( 0.05f );
+			tmp = tmp + 0.05f;
+			tmpAlpha.a = tmp;
+			fader.color = tmpAlpha;
+		}
+
+
 		SceneManager.LoadSceneAsync( "Star_Racer_Intro" );
+
+
 
 
 	}
